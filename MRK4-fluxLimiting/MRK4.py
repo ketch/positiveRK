0c4eecaf724a2f56ccd1a7cd0b6c5922d1ef7d9e
mquezada <manuel.quezada@kaust.edu.sa>
2019-06-07 12:34:08 +0300

    updated working code for modified RK4 with limiting on last stage

--- MRK4-fluxLimiting/MRK4.py
+++ MRK4-fluxLimiting/MRK4.py
@@ -1,59 +1,103 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# These functions are designed for a 1D periodic domain from [0,1] #
+order=2
+L=1.0
 
+# These functions are designed for a 1D periodic domain from [0,L] #
+
+######################################################
+# COMPUTE MATRICES: TRANSPORT AND DISCRETE UPWINDING #
+######################################################
 def compute_matrices(U,N):
-    h=1.0/(N-1)
-    D1=np.diag(np.ones(N-1),-1)-np.diag(np.ones(N-1),1)
-    D1[0,-1]=1.    
-    D1[-1,0]=-1.
-    D1*=U/2.0/h
-
-    # MPP artificial viscosity operator #
+    dtMax = 1.0e10
+    h=L/N
+    if order==2:
+        D1=np.diag(np.ones(N-1),1)-np.diag(np.ones(N-1),-1)
+        D1[0,-1]=-1.
+        D1[-1,0]=1
+        D1*=-U/2.0/h
+    else:
+        D1=-np.diag(np.ones(N-2),2) + 8*np.diag(np.ones(N-1),1) - 8*np.diag(np.ones(N-1),-1) + np.diag(np.ones(N-2),-2)
+        D1[0,-2]=1; D1[0,-1]=-8
+        D1[1,-1]=1
+        D1[-1,0]=8; D1[-1,1]=-1
+        D1[-2,0]=-1
+        D1*=-U/12.0/h
+    #
+    # MPP artificial viscosity operator; i.e, discrete upwinding matrix #
     DL=D1*0
     for i in range(N):
         for j in np.arange(i+1,N):
             DL[i,j]=np.max([-D1[i,j],0,-D1[j,i]])
             DL[j,i]=DL[i,j]
         DL[i,i]=-np.sum(DL[i,:])
-        
-    return D1,DL
+
+        dtMax = min(dtMax,1.0/np.abs(D1[i,i]+DL[i,i]))
+    return D1,DL,dtMax
 #
 
+##################################################################
+# jVector: this determines the sparsity pattern of the operators #
+##################################################################
+def get_jVector(i,D1):
+    if order==2:
+        jVector = [len(D1)-1,i,i+1] if i==0 else ([i-1,i,0] if i==len(D1)-1 else [i-1,i,i+1])
+    else:
+        if i==0:
+            jVector = [len(D1)-2,len(D1)-1,i,i+1,i+2]
+        elif i==1:
+            jVector = [len(D1)-1,i-1,i,i+1,i+2]
+        elif i == len(D1)-1:
+            jVector = [i-2,i-1,i,0,1]
+        elif i == len(D1)-2:
+            jVector = [i-2,i-1,i,i+1,0]
+        else:
+            jVector = [i-2,i-1,i,i+1,i+2]
+        #
+    #
+    return jVector
+#
+
+##########################
+# SPATIAL DISCRETIZATION #
+##########################
 def sdisc(u,D1,DL,compute_flux_matrices=True):
     fG = np.dot(D1+DL,u)
     fB = np.dot(-DL,u)
-    
+
     fijG = D1*0
     fijB = D1*0
-    
+
     if compute_flux_matrices:
         for i in range(len(D1)):
-            jVector = [len(D1)-1,i+1] if i==0 else ([0,i-1] if i==len(D1)-1 else [i-1,i+1])
+            jVector = get_jVector(i,D1)
             for j in jVector:
                 fijG[i,j] = D1[i,j]*u[j]-D1[j,i]*u[i] + DL[i,j]*(u[j]-u[i])
-                fijB[i,j] = -DL[i,j]*(u[j]-u[i])                
+                fijB[i,j] = -DL[i,j]*(u[j]-u[i])
             #
         #
     #
     return fG+fB,fG,fB,fijG,fijB
 #
 
-def limited_non_conservative_flux(dt,un,fG,fB,global_bounds=False,single_gamma=False):
+#################################
+# LIMITED NON-CONSERVATIVE FLUX #
+#################################
+def limited_non_conservative_flux(dt,un,fG,fB,global_bounds=False,single_gamma=False,gmin=0.0,gmax=1.0):
     gamma=0*fB
     for i in range(len(fB)):
-        # compute bounds 
+        # compute bounds
         if global_bounds:
-            umax=1
-            umin=0
+            umax=gmax
+            umin=gmin
         else:
-            jVector = [len(un)-1,i,i+1] if i==0 else ([i-1,i,0] if i==len(un)-1 else [i-1,i,i+1])
+            jVector = get_jVector(i,un)
             for j in jVector:
                 umax=max(umax,un[j])
                 umin=min(umin,un[j])
             #
-        # compute non-conservative limiters 
+        # compute non-conservative limiters
         gamma_neg = (umin-un[i]-dt*fG[i])/dt/fB[i] if (un[i]+dt*(fG[i]+fB[i])<umin and fB[i]!=0) else 1.0
         gamma_pos = (umax-un[i]-dt*fG[i])/dt/fB[i] if (un[i]+dt*(fG[i]+fB[i])>umax and fB[i]!=0) else 1.0
         gamma[i]=min(gamma_neg,gamma_pos)
@@ -65,44 +109,82 @@ def limited_non_conservative_flux(dt,un,fG,fB,global_bounds=False,single_gamma=F
     return sfB
 #
 
-def limited_conservative_flux(un,uL,fij,global_bounds=False):
+###############################################
+# LIMITED CONSERVATIVE FLUX VIA ITERATIVE FCT #
+###############################################
+def limited_conservative_flux(un,uL,fij,global_bounds=False,gmin=0.0,gmax=1.0,num_iter=1):
+    tol=1.0e-12
     min_Lij=1.0e10
-    umax=-1.0e10; umin=1.0e10
     Rpos=un*0; Rneg=un*0; limited_flux_correction=un*0
+    umax=un*0-1.0e10; umin=un*0+1.0e10
+
+    fluxMatrix = np.copy(fij)
+    limitedFlux= fij*0
+    uLim = np.copy(uL)
+
+    ######################
+    # compute the bounds #
+    ######################
     for i in range(len(un)):
         # compute bounds
         if global_bounds:
-            umax=1.; umin=0.
+            umin[i]=gmin
+            umax[i]=gmax
         else:
-            jVector = [len(un)-1,i,i+1] if i==0 else ([i-1,i,0] if i==len(un)-1 else [i-1,i,i+1])
+            jVector = get_jVector(i,un)
             for j in jVector:
-                umax=max(umax,un[j])
-                umin=min(umin,un[j])
+                umax[i]=max(umax[i],un[j])
+                umin[i]=min(umin[i],un[j])
             #
             # check uL
-            assert(umin<=uL[i] and uL[i]<=umax),"Error: low-order solutin is not on bounds"
+            if (uL[i]-umin[i]<-tol or uL[i]-umax[i]>tol):
+                print (umin[i], uL[i],umax[i])
+                assert(umin[i]<=uL[i] and uL[i]<=umax[i]),"Error: low-order solutin is not on bounds"
+            #
         #
-        jVector = [len(un)-1,i+1] if i==0 else ([0,i-1] if i==len(un)-1 else [i-1,i+1])
-        Ppos=0; Pneg=0
-        for j in jVector:
-            # compute p vectors 
-            Ppos += max(fij[i,j],0)
-            Pneg += min(fij[i,j],0)
-        # Compute R vectors 
-        Rpos[i] = min(1.,(umax-uL[i])/Ppos) if Ppos!=0 else 1.0
-        Rneg[i] = min(1.,(umin-uL[i])/Pneg) if Pneg!=0 else 1.0
     #
-    # compute limited flux 
-    for i in range(len(un)):
-        jVector = [len(un)-1,i+1] if i==0 else ([i-1,0] if i==len(un)-1 else [i-1,i+1])
-        for j in jVector:
-            Lij = min(Rpos[i],Rneg[j]) if fij[i,j]>0 else min(Rneg[i],Rpos[j])
-            min_Lij = min(min_Lij,Lij)
-            limited_flux_correction[i] += Lij*fij[i,j]
+    #################
+    # Iterative FCT #
+    #################
+    for iter in range(num_iter):
+        for i in range(len(un)):
+            Pposi=0; Pnegi=0
+            jVector = get_jVector(i,un)
+            # compute p vectors
+            for j in jVector:
+                fluxij = fluxMatrix[i,j] - limitedFlux[i,j]
+                Pposi += max(fluxij,0)
+                Pnegi += min(fluxij,0)
+            #
+            # Compute Q vectors
+            uLimi = uLim[i]
+            Qposi = umax[i]-uLimi
+            Qnegi = umin[i]-uLimi
+
+            # Compute R vectors
+            Rpos[i] = min(1.,Qposi/Pposi) if Pposi!=0 else 1.0
+            Rneg[i] = min(1.,Qnegi/Pnegi) if Pnegi!=0 else 1.0
+        #
+        # compute limited flux
+        for i in range(len(un)):
+            ith_limiter_times_fluxCorrectionMatrix =0.
+            jVector = get_jVector(i,un)
+            for j in jVector:
+                fluxij = fluxMatrix[i,j] - limitedFlux[i,j]
+                Lij = min(Rpos[i],Rneg[j]) if fluxij>0 else min(Rneg[i],Rpos[j])
+
+                # Compute the limited flux
+                ith_limiter_times_fluxCorrectionMatrix += Lij*fluxij
+
+                # Update vectors for next FCT Iteration #
+                limitedFlux[i,j] = Lij*fluxij
+                fluxMatrix[i,j] = fluxij
+
+                min_Lij = min(min_Lij,Lij)
+            #
+            uLim[i] += ith_limiter_times_fluxCorrectionMatrix
+            limited_flux_correction[i] += ith_limiter_times_fluxCorrectionMatrix
         #
     #
     return limited_flux_correction,min_Lij
 #
-
-
-
---
+++ MRK4-fluxLimiting/run_convergence.py
@@ -0,0 +1,207 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from MRK4 import *
+
+def run_MRK4(N,
+             name=None,
+             smooth=0,
+             modify_RK=True,
+             save_solution=None,
+             reference_solution=None,
+             dt_factor=1.0,
+             verbose=0,
+             global_bounds=False,
+             gmin=0.0,
+             gmax=1.0,
+             num_iter=1):
+    
+    #######################
+    # physical parameters #
+    #######################
+    L=1.0
+    U=1.0
+    T=1.0
+    DT=0.1 # frequency for outputting
+    # domain #
+    xFull=np.linspace(0,L,N+1)
+    x=xFull[0:-1]
+
+    ########################
+    # Numerical parameters #
+    ########################
+    h=L/(N) #if periodic: =L/N. Otherwise =L/(N-1)
+    # compute matrices
+    (D1,DL,dtMax)=compute_matrices(U,N)
+    # compute time step
+    C=0.8
+    dt = C*dtMax
+    dt/=dt_factor #1, 2, 4, 8, 16, 32... reference: 1024
+
+    ######################
+    # Initial conditions #
+    ######################
+    disc = (1.0*(x>0.45)*(x<0.55))
+    sinusoidal = (0.5*(np.sin(2*np.pi*x)+1))
+    tanh = (0.5*(np.tanh((x-0.3)/0.05)+1) - 0.5*(np.tanh((x-0.7)/0.05)+1))
+    u0 = disc if smooth == 0 else (sinusoidal if smooth==1 else tanh)
+    un_MRK4=1.0*u0
+
+    # compute initial mass #
+    initial_mass=h*np.sum(u0)
+
+    #############
+    # Time loop #
+    #############
+    mmin_Lij=1E10
+    time=0
+    aux_counter=0
+    last_time_step=False
+    while(True):
+        # compute time
+        time += dt
+
+        # disc in space for RK4
+        f1,f1G,f1B,f1ijG,f1ijB=sdisc(un_MRK4,D1,DL)
+        f2,f2G,f2B,f2ijG,f2ijB=sdisc(un_MRK4+0.5*dt*f1,D1,DL)
+        f3,f3G,f3B,f3ijG,f3ijB=sdisc(un_MRK4+0.5*dt*f2,D1,DL)
+        f4,f4G,f4B,f4ijG,f4ijB=sdisc(un_MRK4+dt*f3,D1,DL)
+
+        if modify_RK:
+            fG=f1G # good flux is given by forward Euler
+            # conservative RK-FCT #
+            fij=(-5*f1ijG+f1ijB + 2*(f2ijG+f2ijB) +2*(f3ijG+f3ijB) + f4ijG+f4ijB)/6.0
+            sfB,min_Lij = limited_conservative_flux(un_MRK4,un_MRK4+dt*fG,dt*fij,global_bounds=global_bounds,gmin=gmin,gmax=gmax,num_iter=num_iter)
+            mmin_Lij = min(mmin_Lij,min_Lij)
+            unp1_MRK4 = (un_MRK4 + dt*fG) + sfB
+        else:
+            unp1_MRK4 = un_MRK4 + dt/6.0*(f1+2*f2+2*f3+f4)
+        #
+
+        # update old solution
+        un_MRK4 = unp1_MRK4[:]
+
+        # To increase verbosity
+        if (time>=DT*aux_counter) and verbose>=2:
+            print ("\t Time: " + str(time))
+            aux_counter+=1
+        #
+
+        # breaking the time loop #
+        if last_time_step:
+            break
+        #
+        if (time+dt)>=T:
+            last_time_step=True
+            dt=T-time
+            assert (dt>0)
+        #
+    #
+
+    # compute loss of mass
+    loss_MRK4 = (initial_mass - h*np.sum(unp1_MRK4))/initial_mass
+
+    # print errors
+    if verbose>=1:
+        print("\t (min, max) value of MRK4 at final time t=T: " + str(np.min(unp1_MRK4)) + " , " + str(np.max(unp1_MRK4)))
+        #print('\t loss in mass of MRK4='+str(loss_MRK4))
+        #print ('min Lij=', mmin_Lij)
+        print ("\t L1 error with MRK4:  \t\t" + str(np.sum(np.abs(unp1_MRK4-u0))/np.sum(np.abs(u0))))
+        print ("\t L-inf error with MRK4: \t" + str(np.max(np.abs(unp1_MRK4-u0))/np.max(np.abs(u0))))
+    #
+
+    # plot the solution
+    if name is not None:
+        # plot final solutions #
+        plt.figure(figsize=([15,10]))
+        uexact = u0
+        plt.plot(xFull,[i for i in uexact]+[u0[0]],'--k',linewidth=3)
+        plt.plot(xFull,[i for i in unp1_MRK4]+[unp1_MRK4[0]],'-r',linewidth=2)
+        plt.plot(xFull,[i for i in unp1_MRK4]+[unp1_MRK4[0]],'*',linewidth=2)
+        plt.legend(['Exact','Num Sol.'])
+        plt.savefig(name)
+    #
+
+    # save solution (as reference) or use a reference solution to compute the errors #
+    if save_solution is not None:
+        np.savetxt(save_solution, unp1_MRK4, delimiter=",")
+    elif reference_solution is not None:
+        reference = np.genfromtxt(reference_solution, delimiter=",")
+        print ('min,max='+str(unp1_MRK4.min())+" , "+str(unp1_MRK4.max()))
+        error = h*np.sum(np.abs(unp1_MRK4-reference))
+        print ("\t"+str(error))
+    #
+#
+
+##############################
+# ********** MAIN ********** #
+##############################
+num_iter=1 # iterations for flux limiting
+
+# Exact global minimum and maximum of tanh initial condition
+gmin=6.1441739107603865e-06
+gmax=0.99932929973906703
+
+#############################
+# CONVERGENCE IN SPACE-TIME #
+#############################
+if True:
+    print ("* Standard RK4 with smooth initial data *")
+    for N in [21, 41, 81, 161, 321, 641]:
+        run_MRK4(N=N,name=None, #'solN='+str(N)+'.png',
+                 smooth=2,modify_RK=False,verbose=1)
+    #
+
+    print ("* Modified RK4 with global bounds and smooth initial data *")
+    for N in [21, 41, 81, 161, 321, 641]:
+        run_MRK4(N=N,name=None, #'solN='+str(N)+'.png',
+                 smooth=2,modify_RK=True,global_bounds=True,verbose=1,num_iter=num_iter,gmin=gmin,gmax=gmax)
+    #
+
+    print ("* Modified RK4 with local bounds and smooth initial data *")
+    for N in [21, 41, 81, 161, 321, 641]:
+        run_MRK4(N=N,name=None, #'solN='+str(N)+'.png',
+                 smooth=2,modify_RK=True,global_bounds=False,verbose=1,num_iter=num_iter)
+    #
+#
+
+############################
+# SELF CONVERGENCE IN TIME #
+############################
+N=21 # sin: 81, tanh: 321
+if False:
+    print ("************************************************")
+    print ("********** Running reference solution **********")
+    print ("************************************************")
+    print ("* Standard RK4 with smooth initial data *")
+    run_MRK4(N=N,name=None,smooth=2,modify_RK=False,save_solution='reference_RK4.csv',dt_factor=1024.0)
+
+    print ("* Modified RK4 with global bounds and smooth initial data *")
+    run_MRK4(N=N,name=None,smooth=2,modify_RK=True,save_solution='reference_MRK4_globalBounds.csv',dt_factor=1024.0,
+             global_bounds=True,gmin=gmin,gmax=gmax,num_iter=num_iter)
+
+    print ("* Modified RK4 with local bounds and smooth initial data *")
+    run_MRK4(N=N,name=None,smooth=2,modify_RK=True,save_solution='reference_MRK4_localBounds.csv',dt_factor=1024.0,
+             global_bounds=False,num_iter=num_iter)
+
+    print ("*******************************************************")
+    print ("********** Running self convergence solution **********")
+    print ("*******************************************************")
+    print ("*... Standard RK4 with smooth initial data ...*")
+    for dt_factor in [1.0,2.0,4.0,8.0,16.0,32.0]:
+        run_MRK4(N=N,name=None, #'sol_dt_factor'+str(int(dt_factor))+'.png',
+                 smooth=2,modify_RK=False,reference_solution='reference_RK4.csv',dt_factor=dt_factor)
+    #
+
+    print ("*... Modified RK4 with global bounds and smooth initial data ...*")
+    for dt_factor in [1.0,2.0,4.0,8.0,16.0,32.0]:
+        run_MRK4(N=N,name=None, #'sol_dt_factor'+str(int(dt_factor))+'.png',
+                 smooth=2,modify_RK=True,reference_solution='reference_MRK4_globalBounds.csv',dt_factor=dt_factor,
+                 global_bounds=True,gmin=gmin,gmax=gmax,num_iter=num_iter)
+    #
+
+    print ("*... Modified RK4 with local bounds and smooth initial data ...*")
+    for dt_factor in [1.0,2.0,4.0,8.0,16.0,32.0]:
+        run_MRK4(N=N,name=None, #'sol_dt_factor'+str(int(dt_factor))+'.png',
+                 smooth=2,modify_RK=True,reference_solution='reference_MRK4_localBounds.csv',dt_factor=dt_factor,
+                 global_bounds=False,num_iter=num_iter)
+#
